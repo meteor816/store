5e2727712a02876eef6f0af9d333359578cc83ca
meteor816 <89378992+meteor816@users.noreply.github.com>
2021-09-16 17:17:12 +0800

    Add files via upload

---
+++ DBUtils.py
@@ -0,0 +1,21 @@
+import pymysql
+host="localhost"
+user="root"
+password="root"
+database="bank"
+def up_date(sql,param):
+    con=pymysql.connect(host=host,user=user,password=password,database=database)
+    cursor=con.cursor()
+    cursor.execute(sql,param)
+    con.commit()
+    cursor.close()
+    con.close()
+
+def select_date(sql,param):
+    con=pymysql.connect(host=host,user=user,password=password,database=database)
+    cursor=con.cursor()
+    cursor.execute(sql,param)
+    con.commit()
+    cursor.close()
+    con.close()
+    return cursor.fetchall()
